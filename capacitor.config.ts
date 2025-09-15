7f94af98bbbed234475761c021acccba67e78a51
shasha-com <sshankari0212@gmail.com>
2025-09-15 12:59:55 +0800

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nyp.myApps',
+  appName: 'myApps',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
